bb62f6fbb7495aa6f1f8b4f0510beea91d393f7f
Ram10 <Rameezmajid10@outlook.com>
2024-10-21 20:43:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/Internetperson-dev/dots"
+progsfile="https://raw.githubusercontent.com/Internetperson-dev/Linux-Desktop-Setup/refs/heads/main/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
